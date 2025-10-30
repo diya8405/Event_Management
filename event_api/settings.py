a38f92ea11f3ef2cb45c433dc039ff284e60213f
diya kansagarae <diyakansagara25@gmail.com>
2025-10-30 19:19:24 +0530

    sloved profile pic error

--- event_api/settings.py
+++ event_api/settings.py
@@ -134,3 +134,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+import os
+
+MEDIA_URL = '/profile_pics/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'profile_pics')
--- event_api/urls.py
+++ event_api/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.authtoken import views as drf_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),  # Include app routes
     path('api/auth/', drf_views.obtain_auth_token),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
